141b382e7212ea86d25ba658390b8eee7512668d
bhabeicg <salkwaenge@gmail.com>
2025-10-23 14:13:07 +0900

    Create Alien.java

---
+++ Alien.java
@@ -0,0 +1,16 @@
+public class Alien extends Sprite {
+    private int hp;
+    private int speed;
+    private int level;
+
+    public Alien(int x, int y, int level) {
+        super(x, y, 5, 5, "alien.png");
+        this.hp = 1 + level / 2;
+        this.speed = 1 + level;
+        this.level = level;
+    }
+
+    public int getHp() { return hp; }
+    public void updateHp(int hp) { this.hp = hp; }
+    public int getSpeed() { return speed; }
+}
